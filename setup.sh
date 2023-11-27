770cc990ab9dad35e5524441e41d880831ed4bc4
Ishtiaque Ahmed CSEH Rafin <94455536+CsehRafin@users.noreply.github.com>
2023-11-27 22:10:27 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
-wget -o remotedesktop.deb https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg -i remotedesktop.deb
+sudo apt update
+sudo apt upgrade -y
+
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt install ./chrome-remote-desktop_current_amd64.deb -y
 echo Use the command for remote desktop provieded from console
