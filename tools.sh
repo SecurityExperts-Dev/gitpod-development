a17432e29167fdc288a6806b564cb2654ffdb376
Ishtiaque Ahmed CSEH Rafin <94455536+CsehRafin@users.noreply.github.com>
2023-11-27 22:10:06 +0600

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,2 +1,4 @@
 sudo apt install neofetch
 sudo apt install xfce4 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
