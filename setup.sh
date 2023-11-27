4bec35fe31248e20f9981f577e2709d4f6e1c11b
Ishtiaque Ahmed CSEH Rafin <94455536+CsehRafin@users.noreply.github.com>
2023-11-27 21:38:21 +0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+wget -o remotedesktop.deb https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg -i remotedesktop.deb
+echo Use the command for remote desktop provieded from console
